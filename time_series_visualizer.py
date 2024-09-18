c2b147541acfb7a680e22e867cf0e837f8e7b47c
mrzlonla <104435104+mrzlonla@users.noreply.github.com>
2024-09-18 09:24:57 +0100

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,1 +1,52 @@
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/freeCodeCamp/boilerplate-page-view-time-series-visualizer/master/fcc-forum-pageviews.csv', index_col='date', parse_dates=True)
+df = df[(df['value'] >= df['value'].quantile(0.025)) & 
+        (df['value'] <= df['value'].quantile(0.975))]
+import matplotlib.pyplot as plt
+
+def draw_line_plot():
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(df.index, df['value'], color='r')
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+    fig.savefig('line_plot.png')
+    return fig
+def draw_bar_plot():
+    df_bar = df.copy()
+    df_bar['year'] = df_bar.index.year
+    df_bar['month'] = df_bar.index.month_name()
+
+    df_bar = df_bar.groupby(['year', 'month'])['value'].mean().unstack()
+
+    fig = df_bar.plot(kind='bar', figsize=(10, 6)).figure
+    plt.xlabel('Years')
+    plt.ylabel('Average Page Views')
+    plt.legend(title='Months', bbox_to_anchor=(1.05, 1))
+    fig.savefig('bar_plot.png')
+    return fig
+
+def draw_box_plot():
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = df_box['date'].dt.year
+    df_box['month'] = df_box['date'].dt.strftime('%b')
+
+    fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+
+    sns.boxplot(x='year', y='value', data=df_box, ax=axes[0])
+    axes[0].set_title('Year-wise Box Plot (Trend)')
+    axes[0].set_xlabel('Year')
+    axes[0].set_ylabel('Page Views')
+
+    sns.boxplot(x='month', y='value', data=df_box, ax=axes[1])
+    axes[1].set_title('Month-wise Box Plot (Seasonality)')
+    axes[1].set_xlabel('Month')
+    axes[1].set_ylabel('Page Views')
+
+    fig.savefig('box_plot.png')
+    return fig
+
+
 
